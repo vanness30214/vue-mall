d162daf5dbee69f0c1d063fb3a2dcbf0452e6e8d
vanness30214 <vanness@vip.qq.com>
2019-04-17 10:57:44 +0800

    4.17 fix

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
-<template>
+<template >
   <div id="app" class="g-container">
     <div class="g-view-container">
-    	<router-view></router-view>
+      <router-view></router-view>
     </div>
     <div class="g-footer-container">
-    	<c-tabbar/>
+      <c-tabbar/>
     </div>
 
   </div>
