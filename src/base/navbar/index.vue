6912622eb9265292c6812b58d71c01cada84cb26
vanness30214 <vanness@vip.qq.com>
2019-04-17 15:17:31 +0800

    4.17 header组件

---
+++ src/base/navbar/index.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="mine-navbar" >
+    <div class="mine-navbar-left" v-if="$slots.left">
+      <slot name="left"></slot>
+    </div>
+    <div class="mine-navbar-center" v-if="$slots.center">
+      <slot name="center"></slot>
+    </div>
+    <div class="mine-navbar-right" v-if="$slots.right">
+      <slot name="right"></slot>
+    </div>
+    <h1 class="mine-navbar-title" v-if="title" >
+      <span class="mine-navbar-text" v-text="title"></span>
+    </h1>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'Me',
+    props: {
+      title: {
+        type: String,
+        default: ''
+      }
+    }
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import "~assets/scss/mixins";
+
+  .mine-navbar {
+    @include flex-between();
+    height: 50px;
+    background-color: #fff;
+    position: relative;
+
+    &-left {
+      margin-left: 10px;
+      ~ .mine-navbar-right{
+        position: static;
+      }
+    }
+
+    &-center {
+      margin: 0 10px;
+      flex: 1;
+      ~ .mine-navbar-right{
+        position: static;
+      }
+    }
+
+    &-right {
+      margin-right: 10px;
+      position: absolute;
+      right: 0;
+      @include flex-center();
+      height: 100%;
+    }
+    &-title {
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 20%;
+      right: 20%;
+      @include flex-center();
+      text-align: center;
+    }
+    &-text{
+      line-height: 1.5;
+      width: 100%;
+      font-size: 18px;
+      @include ellipsis();
+    }
+
+  }
+</style>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,17 +1,32 @@
 <template>
   <div class="home">
-    home
+    <header class="g-header-container">
+      <home-header></home-header>
+    </header>
+    <div></div>
+    <div class="g-backtop-container"></div>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+  import HomeHeader from './header';
+
   export default {
-    name: 'Home'
+    name: 'Home',
+    components: {
+      HomeHeader
+    }
 
   };
 </script>
 
 <style lang="scss" scoped>
-
+  @import '~assets/scss/mixins';
+  .home{
+    overflow: hidden;
+    width: 100%;
+    height: 100%;
+    background-color: $bgc-theme;
+  }
 </style>
