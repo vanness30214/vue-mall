4aaccd47fb300cc299929737d4e11355cf14b76a
vanness30214 <vanness@vip.qq.com>
2019-04-20 10:08:06 +0800

    4.20 CATEGORY

--- src/pages/category/tab.vue
+++ src/pages/category/tab.vue
@@ -1,27 +1,45 @@
 <template>
-  <div>
-    tab
-  </div>
-  <!--<me-scroll>
+  <me-scroll :scrollbar="false">
     <ul class="tab">
-      <li class="tab-item"></li>
+      <li class="tab-item"
+          :class="{'tab-item-active' : item.id === curId}"
+          v-for="(item,index) in items"
+          :key="index"
+          @click="SwitchTab(item.id)"
+      >{{item.name}}</li>
     </ul>
-  </me-scroll>-->
+  </me-scroll>
 </template>
-
 <script>
   import MeScroll from 'base/scroll';
-  
-
+  import {categoryNames} from './config';
   export default {
     name: 'CategoryTab',
+    data() {
+      return {
+        curId: ''
+      };
+    },
+    mounted() {
+      this.init();
+    },
+    methods: {
+      init() {
+        this.items = categoryNames;
+      },
+      SwitchTab(id) {
+        if (this.curId === id) {
+          return;
+        }
+        this.curId = id;
+        this.$emit('switch-tab', id);
+      }
+    },
     components: {
       MeScroll
     }
-    
   };
 </script>
 
 <style lang="scss" scoped>
- 
 </style>
